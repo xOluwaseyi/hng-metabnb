7711efa712a1271a7dd3c35e1c77c276687a1264
Fagbemi Oluwaseyi <seyifagbemi211@gmail.com>
2022-11-14 20:06:09 +0100

    feat: customized error page

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Footer from "./components/UI/Footer";
+import Header from "./components/UI/Header";
 
 const ErrorPage = () => {
   return (
     <div>
-      <p>Sorry this page does not exist</p>
-      <p>
-        {" "}
-        Go
-        <Link to="/">Home</Link> from here.
-      </p>
+      <Header />
+      <div className="h-[600px] grid place-items-center">
+        <div className="w-[80%] lg:w-auto">
+          <p className="text-3xl lg:text-4xl font-bold text-center">Sorry, this page does not exist!</p>
+          <p className="text-center mt-5 text-2xl lg:text-3xl ">
+            Go{" "}
+            <Link to="/" className="bg-[#A02279] px-5 py-1 mx-1 text-white">
+              Home
+            </Link>{" "}
+            from here.
+          </p>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
