c55e0c8a97c2ac3334f623015368096e506c13e5
Fagbemi Oluwaseyi <seyifagbemi211@gmail.com>
2022-11-14 20:54:48 +0100

    styling: added little styling to error page

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -9,10 +9,12 @@ const ErrorPage = () => {
       <Header />
       <div className="h-[600px] grid place-items-center">
         <div className="w-[80%] lg:w-auto">
-          <p className="text-3xl lg:text-4xl font-bold text-center">Sorry, this page does not exist!</p>
-          <p className="text-center mt-5 text-2xl lg:text-3xl ">
+          <p className="text-3xl lg:text-5xl font-bold text-center text-[#A02279]">
+            Sorry, this page does not exist!
+          </p>
+          <p className="text-center mt-10 text-2xl lg:text-3xl ">
             Go{" "}
-            <Link to="/" className="bg-[#A02279] px-5 py-1 mx-1 text-white">
+            <Link to="/" className="bg-[#A02279] px-3 py-1 mx-1 text-white">
               Home
             </Link>{" "}
             from here.
